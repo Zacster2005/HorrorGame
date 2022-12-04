314a31d146581f1500e6d33c5f1e7fba4f842647
Zacster2005 <zacster2005@gmail.com>
2022-12-03 19:05:44 -0500

    Fixed Flashlight Finally

--- HorrorGame/Assets/Scripts/Flashlight.cs
+++ HorrorGame/Assets/Scripts/Flashlight.cs
@@ -5,32 +5,47 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public GameObject Light;
-    bool LightOn;
+    public bool LightOn;
+    public bool Wait = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LightOn = true;
+        Wait = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1))
-        {
-            if(LightOn)
-            {
-          
-                Light.SetActive(false);
-                LightOn = false;
-            }
-
-            if(LightOn)
-            {
-              Light.SetActive(true);
-                LightOn = true;
-            }
+        //Input works
+        if (Input.GetMouseButton(0) && LightOn == true && Wait)
+        {      
+            Light.SetActive(false);
+            LightOn = false;
+            Wait = false;
+            StartCoroutine(FailSafe());
+            Debug.Log("light is off");
         }
 
+        if (Input.GetMouseButton(0) && LightOn == false && Wait)
+        {
+            Light.SetActive(true);
+            LightOn = true;
+            Wait = false;
+            StartCoroutine(FailSafe());
+            Debug.Log("light is on");
+        }
+
+        
+
+    }//Update
+
+    IEnumerator FailSafe()
+    {
+        yield return new WaitForSeconds(1f);
+        Wait = true;
     }
-}
+
+
+}//class
